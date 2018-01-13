575198625702527e5093f5649494efdac7bc08c1
philliphjhuang <league@WTS15.attlocal.net>
2018-01-13 11:28:16 -0800

    star show

--- src/extra/StarShow.java
+++ src/extra/StarShow.java
@@ -1,5 +1,7 @@
 package extra;
 
+import javax.swing.plaf.FontUIResource;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /*** Teacher's note ***/
@@ -9,50 +11,53 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class StarShow {
 	
-	Robot robot = new Robot("batman");
+	Robot bukbeak = new Robot("batman");
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+			//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
 
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+int star = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+int space = 950;
 		// 8. Make a variable to hold the star size and set it to 25
-
-		// 12. Repeat the steps #19 to #18, 30 times
-
-			// 19. Set the pen width to i 
-	
+int size = 25;
 			// 10. Set the X position of the robot to your X variable
-	
+	bukbeak.setX(star);
 			// 11. Set the Y position of the robot to your Y variable
-	
+	bukbeak.setY(space);
 			// 9. Call the drawStar() method with your star size variable
-	
+	drawStar(size);
 			// 14. Increase the X position by star size. See Figure 2.
-	
+	space-=size;
 			// 15. decrease the Y position by star size. See Figure 3.
-	
+	star+=size;
 			// 16. Increase the star size by 20
-	
+	size+=size;
 			// 17. Turn the robot 12 degrees
+	bukbeak.turn(12);
+	// 12. Repeat the steps #19 to #18, 30 times
 	
+	//19. Set the pen width to i
+	//bukbeak.setPenWidth();
 			// 18. Make each star a different random color like in Figure 4. 
-
+	bukbeak.setPenColor(2, 44, 255);
 	}
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+bukbeak.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
+for (int i = 0; i < 5; i++) {
+	
 
 			// 1. Move the robot the distance of the starSize variable
-	
+	bukbeak.move(starSize);
 			// 3. Turn the robot 144 degrees
-			
+			bukbeak.turn(144);
+} 
 	}
 	
 	public static void main(String[] args) {
