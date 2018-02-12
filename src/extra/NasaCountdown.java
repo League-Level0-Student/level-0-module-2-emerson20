6d32ce01861eed4007ea4911c796612b46f3665c
emerson20 <league@WTS-iMac-11.attlocal.net>
2018-02-11 17:30:04 -0800

    that Was Easy, Sound Track To Life, and Nasa Countdown

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -14,14 +16,15 @@ package extra;
 
 public class NasaCountdown {
 	public static void main(String[] args) {
+	
 		// 2. Use a pop-up to ask the user where to start counting from
-
+String count = JOptionPane.showInputDialog("Where do we start our countdown form?");
 		// 3. Change the countdown to use the new starting point
-		
+		int number = Integer.parseInt(count);
 		// 1. Print a countdown from 10 to 0 on the console
-
+for(int i=number;i >= 0;i--)
 		// 4. Use the speak method to hear the countdown.
-		
+		speak(i+"");
 		// 5. when the counting is done, speak "blastoff!"
 	}
 
--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -11,14 +11,27 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "What mood are you in?", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "stressed", "sad", "happy" }, null);
 		
-		// 2. Their answer is stored in the userMood variable. Print it out.
+		// 2. Theiratch?v=nZamwCZOWHI answer is stored in the userMood variable. Print it out.
 
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
+		String youtube ="https://www.youtube.com/watch?v=nZamwCZOWHI";
 		
-
+        if (userMood==0) {
+        	playVideo(youtube);
+		}
+        String sad = "http://bit.ly/video-for-sad";
+        
+        if (userMood==1) {
+			playVideo(sad);
+		}
+        String happy = "http://bit.ly/video-for-happy";
+        
+        if (userMood==2) {
+			playVideo(happy);
+		}
 		
 		// 4. Play different songs for other moods.
 
