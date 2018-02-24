e1ee5b910d41305c2ff48c15c753a20a73906ee6
emerson20 <league@WTS-IM-17.attlocal.net>
2018-02-24 11:03:17 -0800

    Horoscope

--- src/Horoscopes.java
+++ src/Horoscopes.java
@@ -1,4 +1,73 @@
+import javax.swing.JOptionPane;
 
-public class Horoscopes {
+public class Horoscopes {public static void main(String[] args) {
+	//1: Ask the user for their star sign. (Zodiac Signs)
+String signs = JOptionPane.showInputDialog("What is your zodiac sign? (Only use lower case lettes and make sure your spelling is correct.)");
+	//2: Depending on what they answer, tell them their horoscope
+if (sign.equals("capricorn")) {
+	JOptionPane.showMessageDialog(null, "You are pationate, persistent, and easy going as long as something dose not involve work.");
+}else
+	
+	
+if (sign.equals("pisces")) {
+	JOptionPane.showMessageDialog(null, "You'er mystic, you stand up for what you believ in,  have very strong emotions, and can understand others motives with eas.");
+}else
+		
+		
+if (sign.equals("aquarius")) {
+	JOptionPane.showMessageDialog(null, "You are a visionary, independent, good at interacting with others through observations, and have a forward thinking mind.");		
+}else
+			
+			
+if (sign.equals("aries")) {
+    JOptionPane.showMessageDialog(null, "");				
+}else
+				
+				
+if (sign.equals("taurus")) {
+	JOptionPane.showMessageDialog(null, "");					
+}else
+					
+					
+if (sign.equals("gemini")) {
+	JOptionPane.showMessageDialog(null, "");					
+}else
+						
+						
+if (sign.equals("cancer")) {
+	JOptionPane.showMessageDialog(null, "");						
+}else
+							
+							
+if (sign.equals("leo")) {
+	JOptionPane.showMessageDialog(null, "");							
+}else
+								
+								
+if (sign.equals("virgo")) {
+	JOptionPane.showMessageDialog(null, "");								
+}else
+									
+									
+if (sign.equals("libra")) {
+	JOptionPane.showMessageDialog(null, "");									
+}else
+										
+										
+if (sign.equals("scorpio")) {
+	JOptionPane.showMessageDialog(null, "");										
+}else
+											
+											
+if (sign.equals("sagitarius")) {
+	JOptionPane.showMessageDialog(null, "");											
+}else
+												
+												
+
+	//3: If they enter something else, tell them "That's not a star sign!"
+
+
+}
 
 }
